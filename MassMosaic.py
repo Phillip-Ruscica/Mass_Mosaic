02a3ed70c23e0aa814dd6325c43247083886dd80
Phil <83251507+Phillip-Ruscica@users.noreply.github.com>
2021-04-28 16:09:46 -0400

    Update MassMosaic.py
    Rewrote tool processes into functions. Made pep8 compliant

--- MassMosaic.py
+++ MassMosaic.py
@@ -1,40 +1,67 @@
+#!/usr/bin/env python2.7
+"""
+Description: This Arcgis tool merges all raster tiles in a folder into mosaics
+             of a given size.
+Created by: Phillip Ruscica
+"""
+
+###############################################################################
 import os
 import arcpy
+###############################################################################
 
 
-def main(in_workspace, number_of_tiles, temp_workspace, out_workspace, raster_list):
-    arcpy.env.workspace = in_workspace
-    raster_len = len(raster_list)
-    subclasses = raster_len/number_of_tiles
-
-    # merge them into larger tiles
-    top_bound = number_of_tiles - 1
-    bottom_bound = 0
-    count = 0
-    while count <= subclasses:
-
-        sublist = raster_list[bottom_bound: top_bound]
-
-        arcpy.MosaicToNewRaster_management(sublist, temp_workspace, "tiles" + str(bottom_bound) + str(top_bound) + ".tif", "#", "#", "#", "1")
-
-        top_bound = top_bound + number_of_tiles
-        bottom_bound = bottom_bound + number_of_tiles
-        count = count + 1
-
-    # merge the larger tiles into the final mosiac
-    arcpy.env.workspace = temp_workspace
-    final_list = arcpy.ListRasters()
-    arcpy.MosaicToNewRaster_management(final_list, out_workspace, "finalmosaic.tif", "#", "#", "#", "1")
-
-
-if __name__ == "__main__":
-    in_workspace = arcpy.GetParameterAsText(0)
-    number_of_tiles = arcpy.GetParameterAsText(1)
-    temp_workspace = arcpy.GetParameterAsText(2)
-    out_workspace = arcpy.GetParameterAsText(3)
-
+def get_rasters(in_workspace):
+    '''This function get a list of the rasters within the input folder'''
+    # Set the workspace for the input folder
     arcpy.env.workspace = in_workspace
 
     # get the list of rasters in the in_workspace
     raster_list = arcpy.ListRasters()
-    main(in_workspace, number_of_tiles, temp_workspace, out_workspace, raster_list)
+
+    # Return the list of rasters
+    return raster_list
+
+
+def mosaic_chunks(in_workspace, number_of_tiles, out_workspace, raster_list):
+    """This function loops through the files and mosaics them"""
+    # Set the input workspace
+    arcpy.env.workspace = in_workspace
+
+    # Get the number of chunks needed to be processed
+    num_of_chunks = round(len(raster_list) / int(number_of_tiles))
+
+    # Set the index values for the rasters
+    top_bound = number_of_tiles - 1
+    bottom_bound = 0
+
+    # Initiate the loop for identifying each chunk
+    count = 0
+    while count <= subclasses:
+        # Set the bounds of the chunk
+        sublist = raster_list[bottom_bound: top_bound]
+
+        # Mosaic the chunk
+        arcpy.MosaicToNewRaster_management(sublist,
+                                           temp_workspace,
+                                           "tiles" + str(bottom_bound) +
+                                           str(top_bound) + ".tif",
+                                           "#", "#", "#", "1")
+
+        # Update the indicies
+        top_bound = top_bound + number_of_tiles
+        bottom_bound = bottom_bound + number_of_tiles
+        count = count + 1
+###############################################################################
+
+
+if __name__ == "__main__":
+    in_workspace = arcpy.GetParameterAsText(0)     # Folder containing rasters
+    number_of_tiles = arcpy.GetParameterAsText(1)  # Num of tiles per mosaic
+    out_workspace = arcpy.GetParameterAsText(2)    # Output folder
+
+# Get the rasters for processing
+rasters = get_rasters(in_workspace)
+
+# Mosaic the raster into chunks
+mosaic_chunks(in_workspace, number_of_tiles, out_workspace, rasters)
