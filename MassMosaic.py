c183dda013f2f9acdab1cc511ba98e34db1ccf0f
Phil <83251507+Phillip-Ruscica@users.noreply.github.com>
2021-04-29 15:58:42 -0400

    Made it actually work
    Updated toolbox to remove temp file. Added option to process all at once or in chunks.

--- MassMosaic.py
+++ MassMosaic.py
@@ -8,6 +8,7 @@ Created by: Phillip Ruscica
 ###############################################################################
 import os
 import arcpy
+import re
 ###############################################################################
 
 
@@ -16,12 +17,40 @@ def get_rasters(in_workspace):
     # Set the workspace for the input folder
     arcpy.env.workspace = in_workspace
 
-    # get the list of rasters in the in_workspace
+    # Get the list of rasters in the in_workspace
     raster_list = arcpy.ListRasters()
+
+    # Sort the rasters in the list
+    raster_list.sort(key=natural_keys)
 
     # Return the list of rasters
     return raster_list
 
+
+def natural_keys(string):
+    return [ atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', string) ]
+
+
+def atof(text):
+    try:
+        retval = float(text)
+    except ValueError:
+        retval = text
+    return retval
+
+
+def mosaic_all(in_workspace, number_of_tiles, out_workspace, raster_list):
+    # Set the input workspace
+    arcpy.env.workspace = in_workspace
+    # Update the user as to what is being processed
+
+    arcpy.AddMessage('All rasters are being processed into mosaic.tif')
+
+    # Mosaic the chunk
+    arcpy.MosaicToNewRaster_management(raster_list,
+                                       out_workspace,
+                                       'mosaic.tif',
+                                       "#", "#", "#", "1")
 
 def mosaic_chunks(in_workspace, number_of_tiles, out_workspace, raster_list):
     """This function loops through the files and mosaics them"""
@@ -29,39 +58,60 @@ def mosaic_chunks(in_workspace, number_of_tiles, out_workspace, raster_list):
     arcpy.env.workspace = in_workspace
 
     # Get the number of chunks needed to be processed
-    num_of_chunks = round(len(raster_list) / int(number_of_tiles))
+    num_of_chunks = int(round(len(raster_list) / int(number_of_tiles)))
 
-    # Set the index values for the rasters
-    top_bound = number_of_tiles - 1
+
+    # Convert number_of_tiles to an interget value
+    number_of_tiles = int(number_of_tiles)
+
+    # Set the indicies for the chunks bounds
+    highest_index = number_of_tiles - 1
+    top_bound = number_of_tiles
     bottom_bound = 0
 
-    # Initiate the loop for identifying each chunk
-    count = 0
-    while count <= subclasses:
-        # Set the bounds of the chunk
-        sublist = raster_list[bottom_bound: top_bound]
+    # Process all if the number of tiles larger than the number of tiles
+    if number_of_tiles > len(raster_list):
+        mosaic_all(in_workspace, number_of_tiles, out_workspace, raster_list)
 
-        # Mosaic the chunk
-        arcpy.MosaicToNewRaster_management(sublist,
-                                           temp_workspace,
-                                           "tiles" + str(bottom_bound) +
-                                           str(top_bound) + ".tif",
-                                           "#", "#", "#", "1")
+    else:
+        for chunk in range(num_of_chunks):
+            # Set the bounds of the chunk
+            sublist = raster_list[bottom_bound: top_bound]
 
-        # Update the indicies
-        top_bound = top_bound + number_of_tiles
-        bottom_bound = bottom_bound + number_of_tiles
-        count = count + 1
+            # Set the name of the output raster
+            sublist_name = "tiles" + str(bottom_bound) + '_' + str(top_bound) + ".tif"
+            # Update the user to which chunk is being worked on
+            arcpy.AddMessage("Mosaicing " + str(sublist) + ' into ' + str(sublist_name))
+
+            # Mosaic the chunk
+            arcpy.MosaicToNewRaster_management(sublist,
+                                               out_workspace,
+                                               sublist_name,
+                                               "#", "#", "#", "1")
+            # Update the indicies
+            top_bound += number_of_tiles
+            bottom_bound += number_of_tiles
+
 ###############################################################################
 
 
 if __name__ == "__main__":
     in_workspace = arcpy.GetParameterAsText(0)     # Folder containing rasters
-    number_of_tiles = arcpy.GetParameterAsText(1)  # Num of tiles per mosaic
-    out_workspace = arcpy.GetParameterAsText(2)    # Output folder
+    out_workspace = arcpy.GetParameterAsText(1)    # Output folder
+    mosaic_all_at_once = arcpy.GetParameterAsText(2)
+    number_of_tiles = arcpy.GetParameterAsText(3)  # Num of tiles per mosaic
 
+# Set the input workspace
+arcpy.env.workspace = in_workspace
 # Get the rasters for processing
+
 rasters = get_rasters(in_workspace)
 
-# Mosaic the raster into chunks
-mosaic_chunks(in_workspace, number_of_tiles, out_workspace, rasters)
+if mosaic_all_at_once == 'true':
+    mosaic_all(in_workspace, number_of_tiles, out_workspace, rasters)
+else:
+    if number_of_tiles == None:
+        arcpy.AddMessage("For chunk processing, you need to input a processing interval")
+    else:
+        # Mosaic the raster into chunks
+        mosaic_chunks(in_workspace, number_of_tiles, out_workspace, rasters)
